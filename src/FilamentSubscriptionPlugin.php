da8d91875b81876f5e11104bd264fb82e4910b3e
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-08-10 14:43:22 +0100

    Add support to v3

---
+++ src/FilamentSubscriptionPlugin.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IbrahimBougaoua\FilamentCountryStateCity;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+use IbrahimBougaoua\FilamentSubscription\Pages\ManageSubscriptionPage;
+use IbrahimBougaoua\FilamentSubscription\Pages\PlansPage;
+use IbrahimBougaoua\FilamentSubscription\Resources\FeatureResource;
+use IbrahimBougaoua\FilamentSubscription\Resources\PlanResource;
+use IbrahimBougaoua\FilamentSubscription\Resources\PlanSubscriptionsResource;
+use IbrahimBougaoua\FilamentSubscription\Widgets\SubscriptionsOverview;
+
+class FilamentSubscriptionPlugin implements Plugin
+{
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public function getId(): string
+    {
+        return 'filament-subscription-system';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel
+            ->resources([
+				PlanResource::class,
+				FeatureResource::class,
+				PlanSubscriptionsResource::class,
+            ])
+			->pages([
+				ManageSubscriptionPage::class,
+				PlansPage::class,
+			])
+			->widgets([
+				SubscriptionsOverview::class,
+			]);
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+}
--- src/FilamentSubscriptionServiceProvider.php
+++ src/FilamentSubscriptionServiceProvider.php
@@ -4,44 +4,10 @@ namespace IbrahimBougaoua\FilamentSubscription;
 
 use Filament\PluginServiceProvider;
 use IbrahimBougaoua\FilamentSubscription\Commands\FilamentSubscriptionCommand;
-use IbrahimBougaoua\FilamentSubscription\Pages\ManageSubscriptionPage;
-use IbrahimBougaoua\FilamentSubscription\Pages\PlansPage;
-use IbrahimBougaoua\FilamentSubscription\Resources\FeatureResource;
-use IbrahimBougaoua\FilamentSubscription\Resources\PlanResource;
-use IbrahimBougaoua\FilamentSubscription\Resources\PlanSubscriptionsResource;
-use IbrahimBougaoua\FilamentSubscription\Widgets\SubscriptionsOverview;
 use Spatie\LaravelPackageTools\Package;
 
 class FilamentSubscriptionServiceProvider extends PluginServiceProvider
 {
-    protected array $styles = [
-        'tailwindcss-styles' => __DIR__.'/../dist/css/style.css',
-    ];
-
-    protected array $scripts = [
-        //'tailwindcss-scripts' => __DIR__ . '/../dist/js/tailwindcss.js',
-    ];
-
-    protected array $resources = [
-        PlanResource::class,
-        FeatureResource::class,
-        PlanSubscriptionsResource::class,
-    ];
-
-    protected array $pages = [
-        ManageSubscriptionPage::class,
-        PlansPage::class,
-    ];
-
-    protected array $widgets = [
-        SubscriptionsOverview::class,
-    ];
-
-    public function packageBooted(): void
-    {
-        parent::packageBooted();
-    }
-
     public function configurePackage(Package $package): void
     {
         /*
@@ -55,5 +21,12 @@ class FilamentSubscriptionServiceProvider extends PluginServiceProvider
             ->hasViews()
             ->hasMigration('create_filament_subscriptions_table')
             ->hasCommand(FilamentSubscriptionCommand::class);
+    }
+	
+    public function packageBooted(): void
+    {
+        FilamentAsset::register([
+            Css::make('filament-subscriptions-tailwindcss-styles', __DIR__ . '/../dist/css/style.css'),
+        ], 'filament-subscriptions');
     }
 }
