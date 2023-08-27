3e8fe28a9fc6784198a9d1e2a594b5bbfa402109
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-08-27 15:03:28 +0100

    Change lang

--- resources/lang/en/ui.php
+++ resources/lang/en/ui.php
@@ -10,6 +10,7 @@ return
     'switch_plan' => 'Switch Plan',
     'manage_subscriptions' => 'Manage Subscriptions',
     'plans' => 'Plans',
+    'features' => 'Features',
     'allready_used' => 'Allready Used',
     'the_currently_active_subscription' => 'The currently active subscription is ',
     'your_plan_subscription_has_created_successfully' => 'Your plan subscription has created successfully,it will activated after the payment process,Thanks.',
